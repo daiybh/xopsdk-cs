86d223bf1f9087342893db6da0afdecc95d8289f
dai li <d.li@simplylive.tv>
2021-12-27 10:21:40 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿/**
+ * Hangzhou Westsoft Information Technology Co.,Ltd.
+ * Copyright (c) 1993-2016 All Rights Reserved.
+ * 
+ * 用户： zhouhj
+ * 日期: 2016/5/22
+ * 
+ */
+using System;
+using SimpleJson;
+
+namespace com.foxhis.xop.sample
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			// 平台提供的地址
+			const string url = "http://xop.test.foxhis.com/xmsopen-web/rest";
+			// 平台appkey
+			const string appKey = "XOPTEST";
+			// 平台授权码
+			const string secret = "fcKL3Ib4lESYaz8cMPf";
+			// 使用哪个酒店的身份接入 
+			const string hotelId = "G000001";
+			
+			try {
+				var client = new XopClient(url,appKey,secret,hotelId);
+				// 开始登录平台
+				var rspLogin = client.login();
+				Console.WriteLine("login-response:");
+				Console.WriteLine(SimpleJson.SimpleJson.SerializeObject(rspLogin));
+				if (!XopClient.isResponseSuccess(rspLogin))
+				{
+					Console.WriteLine("ERROR login");
+					return;
+				}
+				
+				// 进行业务处理
+				// 使用 JsonObject按apidoc中的  规格组织对象
+				var request = new JsonObject();
+				request["method"] = "xmsopen.xoptest.xopechotest";
+				request["ver"] = "1.0.0";
+				var jary = new JsonArray();
+				request["params"] = jary;
+				var param = new JsonObject();
+				jary.Add(param);
+				param["rq"] = new DateTime(2006,  1,  7,  10,  10,  10,  0);//DateTime.Now;
+				param["hotelid"] = "H000001";
+				param["ok"] = true;
+				JsonObject rsp = client.execute(request);
+				Console.WriteLine("echoTest-response:");
+				Console.WriteLine(SimpleJson.SimpleJson.SerializeObject(rsp));
+					
+				//登出平台
+				var rspLogout = client.logout();
+				Console.WriteLine("logout-response:");
+				Console.WriteLine(SimpleJson.SimpleJson.SerializeObject(rspLogout));
+	
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("exception:");
+				Console.WriteLine(e.ToString());
+			}
+
+
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+
+ 
+}
---
+++ src/XopClient.cs
@@ -0,0 +1,228 @@
+﻿/**
+ * Hangzhou Westsoft Information Technology Co.,Ltd.
+ * Copyright (c) 1993-2016 All Rights Reserved.
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using SimpleJson;
+namespace com.foxhis.xop
+{
+	/**
+	 * XMS OPEN PLATFORM CLIENT  <br>
+	 * 用于XOP客户端处理，集成了sign的校验功能，POST发送功能等等 <br>
+	 * 
+	 * @author zhouhj
+	 * 
+	 */
+	public class XopClient
+	{
+		/**
+		 * 服务URL
+		 */
+		protected string url;
+		/**
+		 * 客户AppKey
+		 */
+		protected string appKey;
+		/**
+		 * 授权密码
+		 */
+		protected string secret;
+		
+		/**
+		 * 连接session
+		 */
+		protected string sessionKey;
+		/**
+		 * 酒店Id
+		 */
+		protected string hotelId;
+		
+		/**
+		 * 返回信息语种
+		 */
+		protected string locale;
+		
+		public XopClient(string url, string appKey, string secret, string hotelId)
+			: this(url, appKey, secret, hotelId, "zh_CN")
+		{
+		}
+		
+		public XopClient(string url, string appKey, string secret, string hotelId, string locale)
+		{
+			this.url = url;
+			this.appKey = appKey;
+			this.secret = secret;
+			this.sessionKey = null;
+			this.hotelId = hotelId;
+			this.locale = locale;
+		}
+		/**
+		 * 判断是否已经登录
+		 * @return
+		 */
+		public bool isLogin()
+		{
+			return !string.IsNullOrEmpty(sessionKey);
+		}
+		
+		/**
+		 * 登录平台
+		 * @return
+		 * @throws Exception
+		 */
+		public JsonObject login()
+		{
+			JsonObject request = new JsonObject();
+			request["method"] = "xmsopen.public.login";
+			request["ver"] = "1.0.0";
+			request["secret"] = secret;
+			JsonObject response = execute(request);
+			if (isResponseSuccess(response)) {
+				this.sessionKey = response["session"].ToString();
+			}
+			return response;
+		}
+		
+		/**
+		 * 登出平台
+		 * @return
+		 * @throws Exception
+		 */
+		public JsonObject logout()
+		{
+			JsonObject request = new JsonObject();
+			request["method"] = "xmsopen.public.logout";
+			request["ver"] = "1.0.0";
+			JsonObject response = execute(request);
+			this.sessionKey = null;
+			return response;
+		}
+		
+		/**
+		 * 具体业务处理调用
+		 * @param request  请求参数
+		 * @return
+		 * @throws Exception
+		 */
+		public JsonObject execute(JsonObject request)
+		{
+			/**
+			 * 平台参数加载
+			 */
+			request["appkey"] = appKey;
+			request["session"] = sessionKey;
+			request["hotelid"] = hotelId;
+			request["loc"] = locale;
+			request["ts"] = 1477217353;// Convert.ToInt64((DateTime.UtcNow  -  new DateTime(1970,  1,  1,  0,  0,  0,  0)).TotalSeconds);
+			request["sign"] = "";
+			/**
+			 * 创建数字校验
+			 */
+			var values = new List<string>();
+			if(request.ContainsKey("params")){
+				walkRequest(request["params"], ref values);
+				values.Sort();
+			}
+			string signStr = "";
+			signStr += appKey + hotelId;
+			signStr += request["loc"] + request["ts"].ToString();
+			for (int i = 0; i < values.Count; i++) {
+				signStr += values[i];
+			}
+			signStr += secret;
+			Trace.TraceInformation("signStr:{0}", signStr);
+			var md5 = new MD5CryptoServiceProvider() as MD5CryptoServiceProvider;
+			byte[] dataHash = md5.ComputeHash(Encoding.UTF8.GetBytes(signStr));
+			var sb = new StringBuilder();
+			foreach (var b in dataHash) {
+				sb.Append(b.ToString("x2").ToUpper());
+			}
+			request["sign"] = sb.ToString();
+			/**
+			 * POST调用
+			 */
+			string jsonReq = SimpleJson.SimpleJson.SerializeObject(request);
+			Trace.TraceInformation("send request:{0}", jsonReq);
+			string jsonRsp = doPost(url, jsonReq);
+			Trace.TraceInformation("recv response:{0}", jsonRsp);
+			JsonObject response = SimpleJson.SimpleJson.DeserializeObject(jsonRsp) as SimpleJson.JsonObject;
+			return response;
+		}
+		
+		/**
+		 * 响应对象是否是成功
+		 * @param response
+		 */
+		public static bool isResponseSuccess(JsonObject response)
+		{
+			if (response.ContainsKey("success")) {
+				return Convert.ToBoolean(response["success"]);
+			}
+			return false;
+		}
+		
+		/**
+		 * HTTP POST 调用
+		 * @param url
+		 * @param jsonReq
+		 * @return
+		 * @throws Exception
+		 */
+		protected static string doPost(string url, string jsonReq)
+		{
+			string jsonRsp = null;
+			var address = new Uri(url);
+			var webRequest = WebRequest.Create(address) as HttpWebRequest;
+			webRequest.Method = "POST";
+			webRequest.Proxy = null;
+			webRequest.ContentType = "application/json";
+			byte[] byteData = Encoding.UTF8.GetBytes(jsonReq);
+			webRequest.ContentLength = byteData.Length;
+			using (var postStream = webRequest.GetRequestStream()) {
+				postStream.Write(byteData, 0, byteData.Length);
+			}
+			using (var webResponse = webRequest.GetResponse() as HttpWebResponse) {
+				var reader = new StreamReader(webResponse.GetResponseStream());
+				jsonRsp = reader.ReadToEnd();
+			}
+			return jsonRsp;
+		}
+		
+		/**
+		 * 获取JSONObject中所有不为空的值
+		 * 
+		 * @param obj
+		 * @param values
+		 */
+		protected static void walkRequest(object obj, ref List<string> values)
+		{
+			if (obj is JsonObject) {
+				var jso = obj as JsonObject;
+				for (var i = jso.Keys.GetEnumerator(); i.MoveNext();) {
+					String key = i.Current;
+					walkRequest(jso[key], ref values);
+				}
+			} else if (obj is JsonArray) {
+				var ary = obj as JsonArray;
+				for (int i = 0; i < ary.Count; i++) {
+					walkRequest(ary[i], ref values);
+				}
+			} else if (obj != null) {
+				if(obj is DateTime){
+					var dt = (DateTime)obj;
+					long tm = Convert.ToInt64((dt  -  new DateTime(1970,  1,  1,  0,  0,  0,  0)).TotalSeconds);
+					values.Add(tm.ToString().ToUpper());
+				}else{
+					values.Add(obj.ToString().ToUpper());
+				}
+			}
+
+		}
+	}
+}
